8627ae9acd7ed6205340ff378ed046238d34d967
mariah <mariah@gardziola.com>
2019-05-30 15:41:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |attendee|
+    puts badge_maker(attendee)
+  end
+end
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end
