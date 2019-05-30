d06b7b4a084326742544c3bb8a57f51afb6a7536
mariah <mariah@gardziola.com>
2019-05-30 15:47:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,8 +18,14 @@ def batch_badge_creator(attendees)
   
 end
 
-def assign_rooms
+def assign_rooms(attendees)
   
+  room = 0 
+  
+  attendees.each do |attendee|
+  return "Hello, #{attendee}! You'll be assigned to room #{room}!"
+  room += 1 
+end
   
 end
 
