997aa7e51fb3e75158e0be2daf14a6a7b0302b4f
mariah <mariah@gardziola.com>
2019-05-30 15:49:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,10 +22,14 @@ def assign_rooms(attendees)
   
   room = 1 
   
+  welcome_messages = Array.new
+  
   attendees.each do |attendee|
-    return "Hello, #{attendee}! You'll be assigned to room #{room}!"
+    welcome_messages << "Hello, #{attendee}! You'll be assigned to room #{room}!"
     room += 1 
   end
+  
+  return welcome_messages
   
 end
 
